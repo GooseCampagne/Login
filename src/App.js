aab1bbc0422c687ec831eae0061b3ce6ac08de44
GooseCampagne <gustavodante202@gmail.com>
2024-07-09 02:17:23 -0600

    9-7-24

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'examen_parcial2' && password === 'Voyporel100') {
+      alert('Inicio de sesión exitoso!');
+      window.location.href = 'https://goosecampagne.projectsutd.online/parcial1/';
+    } else {
+      alert('Credenciales incorrectas. Por favor, intente de nuevo.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="user-box">
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label>Password</label>
+          </div>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
